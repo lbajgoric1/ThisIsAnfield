52cd56a93f76639f1d7e8570fa56f3a3857e0ae5
Lejla Bajgoric <lbajgoric1@etf.unsa.ba>
2016-11-21 19:19:32 +0100

    Galerija urađena preko javascripta, verzija 2.2

--- Javascript/galerija.js
+++ Javascript/galerija.js
@@ -1,14 +1,27 @@
-function prikaziSliku(smSrc, lgSrc) {
-	document.getElementById('velikaSlika').src = smSrc;
-	prikaziVelikiDiv();
-}
-function prikaziVelikiDiv() {
-	document.getElementById('velikiDiv').style.display = 'block';
-}
+// function prikaziSliku(smSrc, lgSrc) {
+	// document.getElementById('velikaSlika').src = smSrc;
+	// prikaziVelikiDiv();
+// }
+// function prikaziVelikiDiv() {
+	// document.getElementById('velikiDiv').style.display = 'block';
+// }
 
-document.onkeydown = function(evt) {
-	evt = evt || window.event;
-	if (evt.keyCode == 27) {
-		document.getElementById("velikiDiv").style.display = "none";
+// document.onkeydown = function(evt) {
+	// evt = evt || window.event;
+	// if (evt.keyCode == 27) {
+		// document.getElementById("velikiDiv").style.display = "none";
+	// }
+// };
+
+function prikazi(id) {
+	var slika = document.getElementById(id);
+	if (slika.requestFullscreen) {
+	  slika.requestFullscreen();
+	} else if (slika.msRequestFullscreen) {
+	  slika.msRequestFullscreen();
+	} else if (slika.mozRequestFullScreen) {
+	  slika.mozRequestFullScreen();
+	} else if (slika.webkitRequestFullscreen) {
+	  slika.webkitRequestFullscreen();
 	}
-};
+}
